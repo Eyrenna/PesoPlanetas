061da885d1853f462e92256d93873c8aa7b5132b
Claudia Gázquez Alessio <claudia365x@gmail.com>
2019-03-24 19:44:10 +0100

    Main working; Añadida salida consola

--- src/main/java/org/lasencinas/Main.java
+++ src/main/java/org/lasencinas/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        double peso = 1.0; /*// kg
+        double peso = 64.0; // kg
 
         for(Planeta planeta : Planeta.values()){
             System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
@@ -20,7 +20,30 @@ public class Main {
         System.out.println("\nYour weight only on the gas giant planets: ");
         for(Planeta planeta: Planeta.getGigantesGaseosos()){
             System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-        }*/
+        }
     }
 
 }
+
+//CONSOLA
+/*Your weight on MERCURY is 24,176487 N
+Your weight on VENUS is 57,919942 N
+Your weight on EARTH is 64,000000 N
+Your weight on MARS is 24,239180 N
+Your weight on JUPITER is 161,955682 N
+Your weight on SATURN is 68,224994 N
+Your weight on URANUS is 57,928141 N
+Your weight on NEPTUNE is 72,852997 N
+
+Your weight only on the terrestrial planets:
+Your weight on MERCURY is 24,176487 N
+Your weight on VENUS is 57,919942 N
+Your weight on EARTH is 64,000000 N
+Your weight on MARS is 24,239180 N
+
+Your weight only on the gas giant planets:
+Your weight on JUPITER is 161,955682 N
+Your weight on SATURN is 68,224994 N
+Your weight on URANUS is 57,928141 N
+Your weight on NEPTUNE is 72,852997 N
+*/
